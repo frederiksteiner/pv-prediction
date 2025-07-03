4a7abba0279369d9d267aa3b6497ba7f80f6d079
Frederik Steiner <steinertrash@gmail.com>
2025-07-03 21:21:36 +0200

    Add flattened weather response

--- src/pv_prediction/data/meteomatics/schemata.py
+++ src/pv_prediction/data/meteomatics/schemata.py
@@ -1,6 +1,30 @@
+import re
 from datetime import datetime
 
 from pydantic import BaseModel
+
+
+class FlattenedWeather(BaseModel):
+    """Weather Response but flattened with lat, lon and date as must have keys."""
+
+    lat: float
+    lon: float
+    date: datetime
+    wind_speed_10m: float | None = None
+    wind_dir_10m: float | None = None
+    wind_gusts_10m_1h: float | None = None
+    wind_gusts_10m_24h: float | None = None
+    t_2m: float | None = None
+    t_max_2m_24h: float | None = None
+    t_min_2m_24h: float | None = None
+    msl_pressure: float | None = None
+    precip_1h: float | None = None
+    precip_24h: float | None = None
+    weather_symbol_1h: int | None = None
+    weather_symbol_24h: int | None = None
+    uv: int | None = None
+    sunrise: datetime | None = None
+    sunset: datetime | None = None
 
 
 class DateValue(BaseModel):
@@ -33,3 +57,72 @@ class WeatherResponse(BaseModel):
     dateGenerated: datetime
     status: str
     data: list[DataParameter]
+
+    def flatten_response(self) -> list[FlattenedWeather]:
+        """Flattens weather response."""
+        flattened_dict = {}
+        for param in self.data:
+            parameter = param.parameter
+            for coord in param.coordinates:
+                for date_value in coord.dates:
+                    key = f"{coord.lat}-{coord.lon}-{date_value.date}"
+                    if key not in flattened_dict:
+                        flattened_dict[key] = {
+                            "lat": coord.lat,
+                            "lon": coord.lon,
+                            "date": date_value.date,
+                        }
+                    changed_param, transformed_value = self._preprocess_params(
+                        parameter, date_value.value
+                    )
+                    flattened_dict[key] = flattened_dict[key] | {
+                        changed_param: transformed_value
+                    }
+        return [FlattenedWeather(**value) for value in flattened_dict.values()]
+
+    @classmethod
+    def _preprocess_params(
+        cls, parameter: str, value: float | int | datetime
+    ) -> tuple[str, float | int | datetime]:
+        new_param, unit = cls._split_units(parameter)
+        if isinstance(value, datetime):
+            return new_param, value
+        if parameter.startswith("t"):
+            return new_param, cls._preprocess_temperature(value, unit)
+        if parameter.startswith("wind_gusts"):
+            return new_param, cls._preprocess_wind_speed(value, unit)
+        if parameter.startswith("msl_pressure"):
+            return new_param, cls._preprocess_pressure(value, unit)
+        return new_param, value
+
+    @staticmethod
+    def _split_units(parameter: str) -> tuple[str, str]:
+        splits = parameter.split(":")
+        if len(splits) == 1:
+            return splits[0], ""
+        only_letters_unit = re.sub("[^a-zA-Z]", "", splits[1])
+        return splits[0], only_letters_unit
+
+    @staticmethod
+    def _preprocess_wind_speed(value: int | float, unit: str) -> float | int:
+        if unit == "kmh":
+            return value / 3.6
+        if unit == "kn":
+            return value / 1.94384001
+        if unit == "bft":
+            return value * 0.836
+        return value
+
+    @staticmethod
+    def _preprocess_temperature(value: int | float, unit: str) -> float | int:
+        if unit == "F":
+            return (value - 32) / 1.8
+        if unit == "K":
+            return value - 273.15
+        return value
+
+    @staticmethod
+    def _preprocess_pressure(value: int | float, unit: str) -> float | int:
+        if unit == "Pa":
+            return value / 100
+        return value
---
+++ tests/pv_prediction/data/meteomatics/test_schemata.py
@@ -0,0 +1,124 @@
+import unittest
+from datetime import datetime
+
+from pv_prediction.data.meteomatics.schemata import FlattenedWeather
+from pv_prediction.data.meteomatics.schemata import WeatherResponse
+
+# pylint: disable=protected-access
+
+
+class TestWeatherResponse(unittest.TestCase):
+    def test_flatte_response(self) -> None:
+        response = {
+            "version": "3.0",
+            "user": "-_steiner_frederik",
+            "dateGenerated": "2025-06-29T07:53:41Z",
+            "status": "OK",
+            "data": [
+                {
+                    "parameter": "t_2m:C",
+                    "coordinates": [
+                        {
+                            "lat": 30.556,
+                            "lon": 5.693083,
+                            "dates": [
+                                {"date": "2025-06-28T22:00:00Z", "value": 20.1},
+                                {"date": "2025-06-29T00:00:00Z", "value": 18.2},
+                            ],
+                        }
+                    ],
+                },
+                {
+                    "parameter": "uv:idx",
+                    "coordinates": [
+                        {
+                            "lat": 30.556,
+                            "lon": 5.693083,
+                            "dates": [
+                                {
+                                    "date": "2025-06-28T22:00:00Z",
+                                    "value": 1,
+                                },
+                                {
+                                    "date": "2025-06-29T00:00:00Z",
+                                    "value": 2,
+                                },
+                            ],
+                        }
+                    ],
+                },
+            ],
+        }
+
+        response = WeatherResponse(**response) # pyre-ignore[6]
+        result = response.flatten_response()
+        self.assertEqual(
+            [
+                FlattenedWeather(
+                    lat=30.556,
+                    lon=5.693083,
+                    date="2025-06-28T22:00:00Z", # pyre-ignore[6]
+                    t_2m=20.1,
+                    uv=1,
+                ),
+                FlattenedWeather(
+                    lat=30.556,
+                    lon=5.693083,
+                    date="2025-06-29T00:00:00Z", # pyre-ignore[6]
+                    t_2m=18.2,
+                    uv=2,
+                ),
+            ],
+            result,
+        )
+
+    def test_preprocess_params_temperature(self) -> None:
+        self.assertEqual(("t_2m", 1), WeatherResponse._preprocess_params("t_2m:C", 1))
+        self.assertEqual(("t_2m", 1), WeatherResponse._preprocess_params("t_2m", 1))
+        self.assertEqual(
+            ("t_2m", -272.15), WeatherResponse._preprocess_params("t_2m:K", 1)
+        )
+        self.assertEqual(("t_2m", 0), WeatherResponse._preprocess_params("t_2m:F", 32))
+
+    def test_preprocess_params_pressure(self) -> None:
+        self.assertEqual(
+            ("msl_pressure", 1),
+            WeatherResponse._preprocess_params("msl_pressure:hPa", 1),
+        )
+        self.assertEqual(
+            ("msl_pressure", 1), WeatherResponse._preprocess_params("msl_pressure", 1)
+        )
+        self.assertEqual(
+            ("msl_pressure", 0.01),
+            WeatherResponse._preprocess_params("msl_pressure:Pa", 1),
+        )
+
+    def test_preprocess_params_wind(self) -> None:
+        self.assertEqual(
+            ("wind_gusts", 1),
+            WeatherResponse._preprocess_params("wind_gusts:km/h", 3.6),
+        )
+        self.assertEqual(
+            ("wind_gusts", 1), WeatherResponse._preprocess_params("wind_gusts:ms", 1)
+        )
+        self.assertEqual(
+            ("wind_gusts", 1), WeatherResponse._preprocess_params("wind_gusts:m/s", 1)
+        )
+        self.assertEqual(
+            ("wind_gusts", 1), WeatherResponse._preprocess_params("wind_gusts", 1)
+        )
+        self.assertEqual(
+            ("wind_gusts", 0.836),
+            WeatherResponse._preprocess_params("wind_gusts:bft", 1),
+        )
+        self.assertEqual(
+            ("wind_gusts", 0.5144456307389208),
+            WeatherResponse._preprocess_params("wind_gusts:kn", 1),
+        )
+
+    def test_preprocess_params_other(self) -> None:
+        self.assertEqual(("asdf", 1), WeatherResponse._preprocess_params("asdf:h", 1))
+        dtm = datetime(2025, 12, 10, 10, 10, 10)
+        self.assertEqual(
+            ("asdf", dtm), WeatherResponse._preprocess_params("asdf:h", dtm)
+        )
